462edde6319bd15c48eac22aa5109726e4d9f876
Carley Tripp <carley.tripp@outlook.com>
2021-06-25 21:26:11 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -2,9 +2,11 @@
 class Comment extends Component {
   render() {
     return (
-      <div>
+      <div className="comment">
         {this.props.commentText}
       </div>
     )
   }
 }
+
+export default Comment
